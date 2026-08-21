3f2e666af0084f63128d0d50fad7848b8d0bbb87
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-08-20 23:44:48 -0300

    FEAT: Implementação do endpoint de listagem de médicos + Add DTO.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,13 +3,13 @@ package med.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.dto.medico.DadosCadastroMedicoDTO;
+import med.voll.api.dto.medico.DadosListagemMedicoDTO;
 import med.voll.api.model.Medico;
 import med.voll.api.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -22,5 +22,10 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedicoDTO dados) {  //eu recebo por parametro um DTO e converto para um objeto do tipo medico
         medicoRepository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedicoDTO> listar() { //converto uma lista de medico para uma lista de DadosListagemMedico
+        return medicoRepository.findAll().stream().map(DadosListagemMedicoDTO::new).toList();
     }
 }
