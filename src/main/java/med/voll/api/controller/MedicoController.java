7eeb5df0dabe5e7d60e6ca19cd243a8d4a56b28f
Pedro Paulo <pedropaulo91.calazans@gmail.com>
2026-08-26 23:44:03 -0300

    FEAT: Implementação da rota de detalhar médico.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -60,4 +60,10 @@ public class MedicoController {
 
         return ResponseEntity.noContent().build();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var medico = medicoRepository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosResponseMedicoDTO(medico));
+    }
 }
