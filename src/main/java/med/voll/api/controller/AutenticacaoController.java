64d297364eb6880a2f5d5a81aa14055a62a0c62e
Pedro Paulo <pedropaulo91.calazans@gmail.com>
2026-09-05 23:57:51 -0300

    FEAT: Configurando o método para gerar token + chamando na controller de autenticação.

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -2,6 +2,8 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.dto.usuario.DadosAutenticacaoDTO;
+import med.voll.api.infra.security.TokenService;
+import med.voll.api.model.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,12 +20,15 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager; //dispara o processo de autenticação, chama a autenticação service
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacaoDTO dados) {
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authentication = manager.authenticate(token);
+        var authentication = manager.authenticate(token); //representa o objeto que tem a autenticação e dentro tem o usuario
 
-        return ResponseEntity.ok("");
+        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
     }
 }
 
---
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -0,0 +1,39 @@
+package med.voll.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.model.usuario.Usuario;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    public String gerarToken(Usuario usuario) {
+        try {
+            var algoritmo = Algorithm.HMAC256("123456"); //senha para fazer a geração do token --- String
+            return JWT.create()
+                    .withIssuer("API Voll.med") //Identificar a api dona responsavel pela geração do token -- vai dentro do token
+                    .withSubject(usuario.getLogin()) //pertence a qual usuário
+                    .withClaim("id", usuario.getId()) //id do usuário
+                    .withExpiresAt(dataExpiracao()) //pode usar o token por 2hrs
+                    .sign(algoritmo);
+
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Erro ao gerar token JWT", exception);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
+
+
+//método para geração do token
+//os token que geramos nas apis, é recomendado configurar uma data de expiração para os tokens
