bf3ef7e1a6dbd506e41ac03cf2082b543d996c0a
Pedro Paulo <pedropaulo91.calazans@gmail.com>
2026-09-15 23:05:05 -0300

    FEAT: Implementação para ser verificado o securityFilter e depois o filter do spring + métodos para tratamentos de erros.

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package med.voll.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -22,7 +27,9 @@ public class SecurityConfigurations {
                 .sessionManagement(session -> session.
                         sessionCreationPolicy(SessionCreationPolicy.STATELESS)).authorizeHttpRequests(
                         auth -> auth.requestMatchers(HttpMethod.POST, "/login") //liberando essa URL
-                                .permitAll().anyRequest().authenticated()).build();
+                                .permitAll().anyRequest().authenticated())
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class) //verificando o meu filter 1° depois do spring
+                .build();
     } /// configurar processos de autenticações e autorizações
 
     @Bean
